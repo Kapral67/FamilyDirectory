13d3a54713aba0c21df5df5fa8991d2c351a2b8b
Maxwell Kapral <git@maxkapral.com>
2023-07-07 21:07:33 -0700

    staging handle whitespace in directory paths

--- stage.bash
+++ stage.bash
@@ -6,30 +6,30 @@ STAGE_DIR="$(pwd)"
 
 function clean_maven_local {
   local cur="$(pwd)"
-  cd ~/.m2/repository/org
+  cd "$HOME/.m2/repository/org"
   rm -rf familydirectory
-  cd $cur
+  cd "$cur"
 }
 
 # DynamoDB assets
-cd $STAGE_DIR/assets/familydirectory-ddb-assets
+cd "$STAGE_DIR/assets/familydirectory-ddb-assets"
 ./gradlew build
 ./gradlew publish
 clean_maven_local
 
 # Lambda assets
-cd $STAGE_DIR/assets/familydirectory-lambda-assets
+cd "$STAGE_DIR/assets/familydirectory-lambda-assets"
 ./gradlew build
 ./gradlew publish
 clean_maven_local
 
 # Lambda Function Handler Shaded Jars
-cd $STAGE_DIR/assets/FamilyDirectoryAdminCreateMemberLambda
+cd "$STAGE_DIR/assets/FamilyDirectoryAdminCreateMemberLambda"
 mvn package
 
 # CDK
-cd $STAGE_DIR/CDK
+cd "$STAGE_DIR/CDK"
 mvn package
 
 # Return to current directory
-cd $CURRENT_DIR
+cd "$CURRENT_DIR"
