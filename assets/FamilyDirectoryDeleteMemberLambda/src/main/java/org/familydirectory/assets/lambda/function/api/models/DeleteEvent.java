a56abfe19262092d458b1747b2b8a239e518c1b2
Maxwell Kapral <git@maxkapral.com>
2023-12-01 17:51:03 -0800

    Jackson Needs to Know How to Create Events (CREATE, UPDATE, DELETE)

--- assets/FamilyDirectoryDeleteMemberLambda/src/main/java/org/familydirectory/assets/lambda/function/api/models/DeleteEvent.java
+++ assets/FamilyDirectoryDeleteMemberLambda/src/main/java/org/familydirectory/assets/lambda/function/api/models/DeleteEvent.java
@@ -1,8 +1,13 @@
 package org.familydirectory.assets.lambda.function.api.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.jetbrains.annotations.NotNull;
 
 public
 record DeleteEvent(@JsonProperty("id") @NotNull String id) {
+    @JsonCreator
+    public
+    DeleteEvent {
+    }
 }
