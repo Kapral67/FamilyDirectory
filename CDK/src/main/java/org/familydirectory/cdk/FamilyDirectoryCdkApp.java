22c2e34f1ddea05083a5abcda9e9b16cf05c15e8
Maxwell Kapral <git@maxkapral.com>
2024-01-30 15:03:57 -0800

    bump version (0.5 -> 0.6); prepare v0.6 release notes

--- CDK/src/main/java/org/familydirectory/cdk/FamilyDirectoryCdkApp.java
+++ CDK/src/main/java/org/familydirectory/cdk/FamilyDirectoryCdkApp.java
@@ -19,7 +19,7 @@ import static java.util.Objects.requireNonNull;
 
 public final
 class FamilyDirectoryCdkApp {
-    public static final String VERSION = "0.5";
+    public static final String VERSION = String.valueOf(0.6);
     public static final String DEFAULT_ACCOUNT;
     public static final String DEFAULT_REGION;
     public static final Environment DEFAULT_ENV;
