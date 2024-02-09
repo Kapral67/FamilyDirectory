24daece6fba8a8f1b58546f8e86daba57e9121ae
Maxwell Kapral <git@maxkapral.com>
2024-02-08 18:57:55 -0800

    ready to compile checkpoint

--- AdminClient/src/main/java/org/familydirectory/sdk/adminclient/events/toolkitcleaner/ToolkitCleanerEvent.java
+++ AdminClient/src/main/java/org/familydirectory/sdk/adminclient/events/toolkitcleaner/ToolkitCleanerEvent.java
@@ -1,29 +1,25 @@
 package org.familydirectory.sdk.adminclient.events.toolkitcleaner;
 
-import io.leego.banana.Ansi;
+import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialogBuilder;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import org.familydirectory.assets.ddb.enums.DdbTable;
-import org.familydirectory.assets.ddb.models.member.MemberRecord;
 import org.familydirectory.sdk.adminclient.enums.ByteDivisor;
-import org.familydirectory.sdk.adminclient.events.model.Executable;
+import org.familydirectory.sdk.adminclient.enums.Commands;
 import org.familydirectory.sdk.adminclient.records.ToolkitCleanerResponse;
-import org.familydirectory.sdk.adminclient.utility.Logger;
+import org.familydirectory.sdk.adminclient.utility.S3CrossRegionClient;
+import org.familydirectory.sdk.adminclient.utility.SdkClientProvider;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import software.amazon.awssdk.services.cloudformation.CloudFormationClient;
 import software.amazon.awssdk.services.cloudformation.model.DescribeStacksRequest;
 import software.amazon.awssdk.services.cloudformation.model.DescribeStacksResponse;
 import software.amazon.awssdk.services.cloudformation.model.GetTemplateRequest;
 import software.amazon.awssdk.services.cloudformation.model.Stack;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
@@ -35,96 +31,66 @@ import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
 
 public final
-class ToolkitCleanerEvent implements Executable {
+class ToolkitCleanerEvent implements Runnable {
     private static final int HASH_LENGTH = 64;
     private static final Pattern CFN_TEMPLATE_HASHES_PATTERN = Pattern.compile("[a-f0-9]{%d}".formatted(HASH_LENGTH));
     private static final String ASSET_BUCKET_PREFIX = "cdk";
     private final long[] deletedItems = {0L};
     private final long[] reclaimedBytes = {0L};
-    private final @NotNull S3Client s3Client = S3Client.builder()
-                                                       .crossRegionAccessEnabled(true)
-                                                       .build();
-    private final @NotNull CloudFormationClient cfnClient = CloudFormationClient.create();
-    private final @NotNull Scanner scanner;
+    private final @NotNull WindowBasedTextGUI gui;
 
     public
-    ToolkitCleanerEvent (final @NotNull Scanner scanner) {
+    ToolkitCleanerEvent (final @NotNull WindowBasedTextGUI gui) {
         super();
-        this.scanner = requireNonNull(scanner);
-    }
-
-    @Override
-    public @NotNull
-    Scanner scanner () {
-        return this.scanner;
-    }
-
-    @Override
-    @Deprecated
-    @NotNull
-    public
-    MemberRecord getExistingMember (final @NotNull String message) {
-        throw new UnsupportedOperationException("Existing Members Unavailable; Picket Not Implemented");
-    }
-
-    @Override
-    @Deprecated
-    @NotNull
-    public
-    List<MemberRecord> getPickerEntries () {
-        throw new UnsupportedOperationException("Picker Not Implemented");
-    }
-
-    @Override
-    @Deprecated
-    public
-    void validateMemberEmailIsUnique (final @Nullable String memberEmail) {
-        throw new UnsupportedOperationException("DynamoDbClient Not Implemented");
+        this.gui = requireNonNull(gui);
     }
 
     @Override
     public
     void run () {
-        Logger.customLine("Clean CDK S3 Assets? (Y/n)", Ansi.BOLD, Ansi.BLUE);
-        final String choice = this.scanner.nextLine()
-                                          .trim();
-        if (!choice.isBlank()) {
-            System.out.println();
-            if (choice.equalsIgnoreCase("n")) {
-                return;
+        final MessageDialog msgDialog = new MessageDialogBuilder().setTitle(Commands.TOOLKIT_CLEANER.name())
+                                                                  .setText("Clean CDK S3 Assets?")
+                                                                  .addButton(MessageDialogButton.Yes)
+                                                                  .addButton(MessageDialogButton.No)
+                                                                  .build();
+        if (msgDialog.showDialog(this.gui)
+                     .equals(MessageDialogButton.Yes))
+        {
+            final ToolkitCleanerResponse response = this.cleanObjects(this.extractTemplateHashes(this.getStackNames()));
+            final StringBuilder responseBuilder = new StringBuilder("Deleted Items: %d | ".formatted(response.deletedItems()));
+            final double bytes = response.reclaimed(ByteDivisor.NONE);
+            if (bytes < ByteDivisor.KILO.divisor()) {
+                responseBuilder.append("Reclaimed Space: %d Bytes".formatted(response.reclaimedBytes()));
+            } else if (bytes < ByteDivisor.MEGA.divisor()) {
+                responseBuilder.append("Reclaimed Space: %f KiB".formatted(response.reclaimed(ByteDivisor.KIBI)));
+            } else if (bytes < ByteDivisor.GIGA.divisor()) {
+                responseBuilder.append("Reclaimed Space: %f MiB".formatted(response.reclaimed(ByteDivisor.MEBI)));
+            } else {
+                responseBuilder.append("Reclaimed Space: %f GiB".formatted(response.reclaimed(ByteDivisor.GIBI)));
             }
+            new MessageDialogBuilder().setTitle(Commands.TOOLKIT_CLEANER.name())
+                                      .setText(responseBuilder.toString())
+                                      .addButton(MessageDialogButton.OK)
+                                      .build()
+                                      .showDialog(this.gui);
         }
-
-        final ToolkitCleanerResponse response = this.cleanObjects(this.extractTemplateHashes(this.getStackNames()));
-
-        Logger.custom("", "Deleted Items: %d | ".formatted(response.deletedItems()), Ansi.BOLD, Ansi.PURPLE);
-        final double bytes = response.reclaimed(ByteDivisor.NONE);
-        if (bytes < ByteDivisor.KILO.divisor()) {
-            Logger.customLine("Reclaimed Space: %d Bytes".formatted(response.reclaimedBytes()), Ansi.BOLD, Ansi.PURPLE);
-        } else if (bytes < ByteDivisor.MEGA.divisor()) {
-            Logger.customLine("Reclaimed Space: %f KiB".formatted(response.reclaimed(ByteDivisor.KIBI)), Ansi.BOLD, Ansi.PURPLE);
-        } else if (bytes < ByteDivisor.GIGA.divisor()) {
-            Logger.customLine("Reclaimed Space: %f MiB".formatted(response.reclaimed(ByteDivisor.MEBI)), Ansi.BOLD, Ansi.PURPLE);
-        } else {
-            Logger.customLine("Reclaimed Space: %f GiB".formatted(response.reclaimed(ByteDivisor.GIBI)), Ansi.BOLD, Ansi.PURPLE);
-        }
-
-        System.out.println();
     }
 
     @NotNull
     private
     Set<String> getStackNames () {
+        final CloudFormationClient cfnClient = SdkClientProvider.getSdkClientProvider()
+                                                                .getSdkClient(CloudFormationClient.class);
         final Set<String> stacks = new HashSet<>();
         String nextToken = null;
         do {
             final DescribeStacksResponse response;
             if (isNull(nextToken)) {
-                response = this.cfnClient.describeStacks();
+                response = cfnClient.describeStacks();
             } else {
-                response = this.cfnClient.describeStacks(DescribeStacksRequest.builder()
-                                                                              .nextToken(nextToken)
-                                                                              .build());
+                response = cfnClient.describeStacks(DescribeStacksRequest.builder()
+                                                                         .nextToken(nextToken)
+                                                                         .build());
             }
             nextToken = response.nextToken();
             stacks.addAll(response.stacks()
@@ -140,10 +106,12 @@ class ToolkitCleanerEvent implements Executable {
     Set<String> extractTemplateHashes (final @NotNull Set<String> stackNames) {
         final Set<String> hashes = new HashSet<>();
         stackNames.forEach(stackName -> {
-            final String templateBody = this.cfnClient.getTemplate(GetTemplateRequest.builder()
-                                                                                     .stackName(stackName)
-                                                                                     .build())
-                                                      .templateBody();
+            final String templateBody = SdkClientProvider.getSdkClientProvider()
+                                                         .getSdkClient(CloudFormationClient.class)
+                                                         .getTemplate(GetTemplateRequest.builder()
+                                                                                        .stackName(stackName)
+                                                                                        .build())
+                                                         .templateBody();
             final Matcher hashesMatcher = CFN_TEMPLATE_HASHES_PATTERN.matcher(templateBody);
             while (hashesMatcher.find()) {
                 hashes.add(hashesMatcher.group());
@@ -155,12 +123,14 @@ class ToolkitCleanerEvent implements Executable {
     @NotNull
     private
     ToolkitCleanerResponse cleanObjects (final @NotNull Set<String> assetHashes) {
-        final Set<String> buckets = this.s3Client.listBuckets()
-                                                 .buckets()
-                                                 .stream()
-                                                 .map(Bucket::name)
-                                                 .filter(s -> s.startsWith(ASSET_BUCKET_PREFIX))
-                                                 .collect(Collectors.toUnmodifiableSet());
+        final S3Client s3Client = SdkClientProvider.getSdkClientProvider()
+                                                   .getSdkClient(S3CrossRegionClient.class);
+        final Set<String> buckets = s3Client.listBuckets()
+                                            .buckets()
+                                            .stream()
+                                            .map(Bucket::name)
+                                            .filter(s -> s.startsWith(ASSET_BUCKET_PREFIX))
+                                            .collect(Collectors.toUnmodifiableSet());
         buckets.forEach(bucketName -> {
             String keyMarker = null;
             String versionIdMarker = null;
@@ -169,19 +139,19 @@ class ToolkitCleanerEvent implements Executable {
                                                                                                   .bucket(bucketName);
                 final ListObjectVersionsResponse response;
                 if (isNull(keyMarker) && isNull(versionIdMarker)) {
-                    response = this.s3Client.listObjectVersions(requestBuilder.build());
+                    response = s3Client.listObjectVersions(requestBuilder.build());
                 } else if (isNull(keyMarker)) {
-                    response = this.s3Client.listObjectVersions(requestBuilder.versionIdMarker(versionIdMarker)
-                                                                              .build());
+                    response = s3Client.listObjectVersions(requestBuilder.versionIdMarker(versionIdMarker)
+                                                                         .build());
                 } else if (isNull(versionIdMarker)) {
-                    response = this.s3Client.listObjectVersions(requestBuilder.keyMarker(keyMarker)
-                                                                              .build());
+                    response = s3Client.listObjectVersions(requestBuilder.keyMarker(keyMarker)
+                                                                         .build());
                 } else {
-                    response = this.s3Client.listObjectVersions(ListObjectVersionsRequest.builder()
-                                                                                         .bucket(bucketName)
-                                                                                         .keyMarker(keyMarker)
-                                                                                         .versionIdMarker(versionIdMarker)
-                                                                                         .build());
+                    response = s3Client.listObjectVersions(ListObjectVersionsRequest.builder()
+                                                                                    .bucket(bucketName)
+                                                                                    .keyMarker(keyMarker)
+                                                                                    .versionIdMarker(versionIdMarker)
+                                                                                    .build());
                 }
                 keyMarker = response.nextKeyMarker();
                 versionIdMarker = response.nextVersionIdMarker();
@@ -190,17 +160,17 @@ class ToolkitCleanerEvent implements Executable {
                             final String hash = obj.key()
                                                    .substring(0, HASH_LENGTH);
                             if (!assetHashes.contains(hash)) {
-                                this.reclaimedBytes[0] += this.s3Client.headObject(HeadObjectRequest.builder()
-                                                                                                    .bucket(bucketName)
-                                                                                                    .key(obj.key())
-                                                                                                    .versionId(obj.versionId())
-                                                                                                    .build())
-                                                                       .contentLength();
-                                this.s3Client.deleteObject(DeleteObjectRequest.builder()
-                                                                              .bucket(bucketName)
-                                                                              .key(obj.key())
-                                                                              .versionId(obj.versionId())
-                                                                              .build());
+                                this.reclaimedBytes[0] += s3Client.headObject(HeadObjectRequest.builder()
+                                                                                               .bucket(bucketName)
+                                                                                               .key(obj.key())
+                                                                                               .versionId(obj.versionId())
+                                                                                               .build())
+                                                                  .contentLength();
+                                s3Client.deleteObject(DeleteObjectRequest.builder()
+                                                                         .bucket(bucketName)
+                                                                         .key(obj.key())
+                                                                         .versionId(obj.versionId())
+                                                                         .build());
                                 ++this.deletedItems[0];
                             }
                         });
@@ -208,28 +178,5 @@ class ToolkitCleanerEvent implements Executable {
             } while (nonNull(keyMarker) || nonNull(versionIdMarker));
         });
         return new ToolkitCleanerResponse(this.deletedItems[0], this.reclaimedBytes[0]);
-    }
-
-    @Override
-    @Deprecated
-    @Nullable
-    public
-    Map<String, AttributeValue> getDdbItem (final @NotNull String primaryKey, final @NotNull DdbTable ddbTable) {
-        throw new UnsupportedOperationException("DynamoDbClient Not Implemented");
-    }
-
-    @Override
-    @Deprecated
-    @NotNull
-    public
-    DynamoDbClient getDynamoDbClient () {
-        throw new UnsupportedOperationException("DynamoDbClient Not Implemented");
-    }
-
-    @Override
-    public
-    void close () {
-        this.s3Client.close();
-        this.cfnClient.close();
     }
 }
