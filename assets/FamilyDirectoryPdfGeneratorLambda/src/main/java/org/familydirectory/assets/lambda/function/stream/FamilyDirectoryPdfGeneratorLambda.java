722a8a8227ff2f3ddb65e7fe132ea4c81dbb79cd
Maxwell Kapral <git@maxkapral.com>
2024-09-07 19:12:52 -0700

    fix premature close of output stream when saving pdfs; adds debug logging of ddb stream events

--- assets/FamilyDirectoryPdfGeneratorLambda/src/main/java/org/familydirectory/assets/lambda/function/stream/FamilyDirectoryPdfGeneratorLambda.java
+++ assets/FamilyDirectoryPdfGeneratorLambda/src/main/java/org/familydirectory/assets/lambda/function/stream/FamilyDirectoryPdfGeneratorLambda.java
@@ -1,11 +1,14 @@
 package org.familydirectory.assets.lambda.function.stream;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+import org.familydirectory.assets.lambda.function.stream.IO.IgnoredCloseOutputStream;
 import org.familydirectory.assets.lambda.function.stream.IO.S3ByteArrayOutputStream;
 import org.familydirectory.assets.lambda.function.stream.helper.PdfHelper;
 import org.familydirectory.assets.lambda.function.utility.LambdaUtils;
@@ -13,6 +16,7 @@ import org.jetbrains.annotations.NotNull;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import static com.amazonaws.services.lambda.runtime.logging.LogLevel.DEBUG;
 import static com.amazonaws.services.lambda.runtime.logging.LogLevel.FATAL;
 import static java.lang.System.getenv;
 import static java.util.Objects.requireNonNull;
@@ -23,10 +27,12 @@ class FamilyDirectoryPdfGeneratorLambda implements RequestHandler<DynamodbEvent,
     @Override
     public
     Void handleRequest (final DynamodbEvent dynamodbEvent, final @NotNull Context context) {
+        final LambdaLogger logger = context.getLogger();
         try {
+            logger.log(Objects.toString(dynamodbEvent), DEBUG);
             final String pdfKey;
             final RequestBody pdfRequestBody;
-            try (final PdfHelper pdfHelper = new PdfHelper(context.getLogger())) {
+            try (final PdfHelper pdfHelper = new PdfHelper(logger)) {
                 final String rootMemberSurname = pdfHelper.getRootMemberSurname();
                 pdfKey = pdfHelper.getPdfS3Key(rootMemberSurname);
                 pdfRequestBody = zipPdfBundle(pdfHelper, rootMemberSurname);
@@ -44,22 +50,22 @@ class FamilyDirectoryPdfGeneratorLambda implements RequestHandler<DynamodbEvent,
             return null;
 
         } catch (final Throwable e) {
-            LambdaUtils.logTrace(context.getLogger(), e, FATAL);
+            LambdaUtils.logTrace(logger, e, FATAL);
             throw new RuntimeException(e);
         }
     }
 
     private static @NotNull
     RequestBody zipPdfBundle (final @NotNull PdfHelper pdfHelper, final @NotNull String rootMemberSurname) throws IOException {
-        try (final S3ByteArrayOutputStream bos = new S3ByteArrayOutputStream(); final ZipOutputStream zos = new ZipOutputStream(bos)) {
+        try (final var bos = new S3ByteArrayOutputStream(); final var zos = new ZipOutputStream(bos); final var ios = new IgnoredCloseOutputStream(zos)) {
             final ZipEntry familyDirectoryZip = new ZipEntry("%sFamilyDirectory.pdf".formatted(requireNonNull(rootMemberSurname)));
             zos.putNextEntry(familyDirectoryZip);
-            pdfHelper.saveDirectoryPdf(zos);
+            pdfHelper.saveDirectoryPdf(ios);
             zos.closeEntry();
 
             final ZipEntry birthdayZip = new ZipEntry("%sFamilyBirthdays.pdf".formatted(rootMemberSurname));
             zos.putNextEntry(birthdayZip);
-            pdfHelper.saveBirthdayPdf(zos);
+            pdfHelper.saveBirthdayPdf(ios);
             zos.closeEntry();
 
             return bos.requestBody();
