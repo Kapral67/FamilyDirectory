e98f1ab53378cfc590aa756cf41986994355bb11
Maxwell Kapral <git@maxkapral.com>
2025-03-16 07:27:11 -0700

    compile time version check

--- assets/familydirectory-service-assets/src/main/java/org/familydirectory/assets/Constants.java
+++ assets/familydirectory-service-assets/src/main/java/org/familydirectory/assets/Constants.java
@@ -2,7 +2,7 @@ package org.familydirectory.assets;
 
 public final
 class Constants {
-    public static final Double VERSION = Double.class.cast(BuildProperties.VERSION.getProperty());
+    public static final Double VERSION = (Double) BuildProperties.VERSION.getProperty();
 
     private
     Constants () {
